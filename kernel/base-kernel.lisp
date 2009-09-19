0baaf68d5caee02be72586c4d319b260d838bdb7
Jakob Reschke <jakob@resfarm.de>
2009-09-19 19:00:24 +0200

    Neue Handler für Host, subject in kernel umbenannt
    letzteres bei receive-requests und switch-state

--- kernel/base-kernel.lisp
+++ kernel/base-kernel.lisp
@@ -13,15 +13,15 @@
 (defmethod address-compare-function ((kernel base-kernel))
   (address-compare-function (comm kernel)))
 
-(defmethod receive-requests ((subject base-kernel))
+(defmethod receive-requests ((kernel base-kernel))
   "Holt alle vorliegenden Anfragen aus dem Kommunikationsobjekt heraus und ruft entsprechende Anfragehandler auf."
-  (loop while (comm:has-request (comm subject))
-     do (multiple-value-bind (request-name sender request-args) (comm:get-request (comm subject))
+  (loop while (comm:has-request (comm kernel))
+     do (multiple-value-bind (request-name sender request-args) (comm:get-request (comm kernel))
 	  (apply (handler-fn request-name) sender request-args))))
 
-(defmethod switch-state ((subject base-kernel) target-state)
+(defmethod switch-state ((kernel base-kernel) target-state)
   "Wechselt den Zustand des Kernelobjekts. Dies hat Auswirkungen auf die Menge der Akzeptierten Anfragen."
-  (setf (state subject) target-state))
+  (setf (state kernel) target-state))
 
 ;; Comm teilt die eigene Adresse nach erfolgtem Login mit
 (defhandler own-address (unregistered) (base-kernel comm address)
--- kernel/host.lisp
+++ kernel/host.lisp
@@ -9,6 +9,26 @@
    (want-game-start :accessor want-game-start :initform nil 
 		    :documentation "Liste der Spieler, die eine neue Runde wollen")))
 
+(defhandler login-parameters (start) (host comm parameters)
+  (call-ui login-parameters host comm parameters))
+
+(defhandler login-data (start) (host ui data)
+  (comm:login (comm host) data)
+  (switch-state host 'registration))
+
+(defmethod send-to-players ((host host) request-name &rest request-args)
+  "Sendet einen request an alle registrierten Spieler."
+  (dolist (receiver (registered-players host))
+    (apply #'comm:send (comm host) receiver request-name request-args)))
+
+(defhandler registration-request (registration) (host sender)
+  (if (>= (length (registered-players host)) 3)
+      (comm:send (comm host) sender registration-reply nil)
+      (unless (member sender (registered-players host) :test (address-compare-function host))
+	(send-to-players host server-update `(:player-join ,sender))
+	(push sender (registered-players host)) ; Spieler aufnehmen
+	(comm:send (comm host) sender registration-reply t))))
+
 (defhandler game-start (game-over) (host sender)
   (unless (member sender (want-game-start host) :test (address-compare-function host))
     (push sender (want-game-start host)))
