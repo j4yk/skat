e2d18b1825309fbc2e994a82cb4e69ae0155d857
Jakob Reschke <jakob@resfarm.de>
2009-10-16 21:26:14 +0200

    define-state-switch-function hinzugefügt
    Definiert eine Methode, nach deren Ausführung der Zustand
    des Kernels geändert wird. Die Methode wird mit defkernelmethod
    definiert.
    
    Weiterhin erzeugt das Makro ein neues Makro, dass den Aufruf
    der state-switch-function vereinfachen soll, indem man das
    Kernelobjekt nicht selbst als Parameter angeben muss.

--- kernel/states.lisp
+++ kernel/states.lisp
@@ -10,3 +10,25 @@ für einen bestimmten Zustand zurück."
   "Definiert eine neue Zustandseingangsfunktion."
   `(defmethod ,(state-entering-function state) ((,kernel-class ,kernel-class))
      ,@body))
+
+(defmacro define-state-switch-function (state (kernel-class &rest args) &body body)
+  "Definiert eine Methode, die body ausführt und dann den Kernel in den angegebenen
+Zustand überführt, und ein Makro, welches diese Funktion aus einer Kernelmethode
+(defkernelmethod) oder einem Request-Handler (defhandler) heraus aufrufen kann.
+(define-state-switch-function registration (player <argumente>)
+  ...)
+==> Methode switch-to-registration ((player player) <argumente>),
+    Makro registration
+Benutzung in defhandler oder defkernelmethod: (registration <argumente>)"
+  (let ((methodname (intern (concatenate 'string "SWITCH-TO-" (symbol-name state)))))
+    ;; Argumente auftrennen in normale, key-Argumente und Restargumente
+    (multiple-value-bind (normal-args key-args rest-args) (separate-lambda-list args)
+      `(prog1 
+	   (defkernelmethod ,methodname (,kernel-class ,@args)
+	     ,@(append body `((switch-state kernel ',state)))) ; die eigentliche Methode
+	 ;; jetzt noch das Makro
+	 (defmacro ,state (,@args)
+	   `(apply ,(function ,methodname) kernel ,,@normal-args ; normale Argumente
+		   ,,@(loop for a in key-args ; key-Argumente für den Aufruf umformen
+			append `(,(to-keyword a) ,a))
+		   ,,(car rest-args))))))) ; und den Rest fürs apply nehmen
