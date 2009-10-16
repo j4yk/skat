699efcaefed98f3b8001d775f6bc563afe0f29ac
Jakob Reschke <jakob@resfarm.de>
2009-10-16 21:22:35 +0200

    separate-lambda-list Funktion hinzugefügt
    findet sich in der Datei utils.lisp und zerlegt
    eine Lambda-Liste in normale Argumente,
    Schlüsselwortargumente und den Restparameter

--- utils.lisp
+++ utils.lisp
@@ -9,3 +9,12 @@
   (if (stringp (car body))
       (values (cdr body) (car body))
       (values body nil)))
+
+(defun separate-lambda-list (lambda-list)
+  "Trennt eine Lambda-Liste auf.
+seperate-lambda-list (argument*) ==> normale-Parameter, key-Parameter, rest-Parameter"
+  (let ((pkey (position '&key lambda-list))
+	(prest (position '&rest lambda-list)))
+    (values (subseq lambda-list 0 (or pkey prest))
+	    (when pkey (subseq lambda-list (1+ pkey) prest))
+	    (when prest (subseq lambda-list (1+ prest))))))
