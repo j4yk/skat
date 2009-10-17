4c0cfe0ee503fcb61f48939f919f691326e5164a
Jakob Reschke <jakob@resfarm.de>
2009-10-17 21:36:27 +0200

    fixed define-state-switch-function
    Im Aufrufmakro wurde das Funktionsargument für apply
    nicht in "#'methodenname" expandiert, sondern in das
    konkrete Funktionsobjekt (also die Instanz der Funktion,
    statt der Referenz über ihren Namen)

--- kernel/states.lisp
+++ kernel/states.lisp
@@ -14,12 +14,7 @@ für einen bestimmten Zustand zurück."
 (defmacro define-state-switch-function (state (kernel-class &rest args) &body body)
   "Definiert eine Methode, die body ausführt und dann den Kernel in den angegebenen
 Zustand überführt, und ein Makro, welches diese Funktion aus einer Kernelmethode
-(defkernelmethod) oder einem Request-Handler (defhandler) heraus aufrufen kann.
-(define-state-switch-function registration (player <argumente>)
-  ...)
-==> Methode switch-to-registration ((player player) <argumente>),
-    Makro registration
-Benutzung in defhandler oder defkernelmethod: (registration <argumente>)"
+(defkernelmethod) oder einem Request-Handler (defhandler) heraus aufrufen kann."
   (let ((methodname (intern (concatenate 'string "SWITCH-TO-" (symbol-name state)))))
     ;; Argumente auftrennen in normale, key-Argumente und Restargumente
     (multiple-value-bind (normal-args key-args rest-args) (separate-lambda-list args)
@@ -28,7 +23,7 @@ Benutzung in defhandler oder defkernelmethod: (registration <argumente>)"
 	     ,@(append body `((switch-state kernel ',state)))) ; die eigentliche Methode
 	 ;; jetzt noch das Makro
 	 (defmacro ,state (,@args)
-	   `(apply ,(function ,methodname) kernel ,,@normal-args ; normale Argumente
+	   `(apply ,'#',methodname kernel ,,@normal-args ; normale Argumente
 		   ,,@(loop for a in key-args ; key-Argumente für den Aufruf umformen
 			append `(,(to-keyword a) ,a))
 		   ,,(car rest-args))))))) ; und den Rest fürs apply nehmen
