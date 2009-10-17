573f9206e5bb09552187c55f80b3aff0a09bbd50
Jakob Reschke <jakob@resfarm.de>
2009-10-18 01:32:22 +0200

    viele Tests für compare-cards und same-suit-p, make-load-form für card
    Durch letzteres können #c farbe wert Konstanten im Code nun
    erfolgreich compiliert werden (es ist wie eine Anleitung
    für den Compiler, wie er die Karteninstanzen reproduzieren kann).
    
    Tja die Tests sind hoffentlich sinnvoll.
    Ich sollte jedoch überlegen sie auszulagern.
    
    Weiterhin steht auch im Code ein Vorschlag für ein neues Makro,
    welches die Testerei vereinfachen soll:
    (deftests category
      {(test-name (test-function-name input-argument*) output-form)}*)

--- kernel/cards.lisp
+++ kernel/cards.lisp
@@ -51,6 +51,12 @@
 (defmethod print-card (card stream)
   (format stream "#c ~a ~a" (symbol-name (suit card)) (symbol-name (value card))))
 
+(defmethod make-load-form ((card card) &optional environment)
+  "Stellt eine Form für den Compiler zur Verfügung, um
+identische Kartenobjekte zu erzeugen."
+  (declare (ignore environment))
+  `(make-card :suit ,(suit card) :value ,(value card)))
+
 (defun all-cards ()
   "Gibt alle 32 Karten zurück."
   (loop for suit in (mapcar #'car *card-suits*)
@@ -65,8 +71,34 @@ Trümpfe gelten als eigene Farbe."
       (or (eq (suit card2) game-variant) (eq (value card2) :jack))
       (eq (suit card1) (suit card2))))
 
+(deftest "same-suit-p für Bube und Trumpfkarte" :category "Cards"
+	 :input-form (values #cDA #cHJ :diamonds)
+	 :test-fn #'same-suit-p
+	 :output-form t)
+
+(deftest "same-suit-p für Bube und Nichttrumpfkarte" :category "Cards"
+	 :input-form (values #cDA #cHJ :spades)
+	 :test-fn #'same-suit-p
+	 :output-form nil)
+
+(deftest "same-suit-p für zwei Karten verschiedener Farben" :category "Cards"
+	 :input-form (values #cDA #cHA :spades)
+	 :test-fn #'same-suit-p
+	 :output-form nil)
+
+(deftest "same-suit-p für zwei Karten gleicher Farbe" :category "Cards"
+	 :input-form (values #cD10 #cDA :hearts)
+	 :test-fn #'same-suit-p
+	 :output-form t)
+
+(deftest "same-suit-p für zwei Trumpfkarten" :category "Cards"
+	 :input-form (values #cD10 #cDA :diamonds)
+	 :test-fn #'same-suit-p
+	 :output-form t)
+
 (defmethod compare-cards ((card1 card) (card2 card) (game-variant (eql :null)))
-  "Berechnet einen Vergleichswert der beiden Karten für ein Nullspiel."
+  "Berechnet einen Vergleichswert der beiden Karten für ein Nullspiel.
+Ist der Wert positiv ist die erste Karte höher als die zweite."
   (labels ((position-in-values-order (card)
 	     (position (value card) *values-order-null*)))
     (if (eq (suit card1) (suit card2))
@@ -74,20 +106,50 @@ Trümpfe gelten als eigene Farbe."
 	1)))
 
 (defun jack-p (card)
+  "Gibt t zurück, wenn card ein Bube ist."
   (eq (value card) :jack))
 
-(deftest "compare-cards two jacks" :category "Cards"
-	 :input-form (values #c hearts jack #c diamonds jack)
+(defmethod compare-cards ((card1 card) (card2 card) (game-variant symbol))
+  "Berechnet einen Vergleichswert der beiden Karten für ein Farbenspiel.
+Ist der Wert positiv ist die erste Karte höher als die zweite."
+  (symbol-macrolet ((card1-wins 1)
+		    (card2-wins -1))
+    (if (same-suit-p card1 card2 game-variant)
+	(if (jack-p card1)
+	    (if (jack-p card2)
+		;; beide Karten sind Buben
+		(reduce #'- (list card1 card2) ; Unterschied der Buben ausrechnen
+			:key (lambda (c) (position (suit c) *suit-order-jacks*)))
+		;; beide Karten sind Trumpf, nur die erste ist ein Bube
+		card1-wins)    ; Buben sind immer die höchsten Trümpfe
+	    (if (jack-p card2)
+		;; beide Karten sind Trumpf, jedoch nur die zweite ein Bube
+		card2-wins     ; Buben sind immer die höchsten Trümpfe
+		;; keine der beiden Karten ist ein Bube, aber gleichfarbig
+		(reduce #'- (list card1 card2) ; Unterschied des Kartenwertes ausrechnen
+			:key (lambda (c) (position (value c) *values-order-normal*)))))
+	;; die Karten sind von unterschiedlicher Spielfarbe
+	card1-wins)))
+
+(deftest "compare-cards mit zwei Buben im Farbspiel" :category "Cards"
+	 :input-form (values #c hearts jack #c diamonds jack :diamonds)
 	 :test-fn #'compare-cards
 	 :output-form 1)
 
-(defmethod compare-cards ((card1 card) (card2 card) (game-variant keyword))
-  "Berechnet einen Vergleichswert der beiden Karten für ein Farbenspiel."
-  (if (same-suit-p card1 card2 game-variant)
-      (if (jack-p card1)
-	  (if (jack-p card2)
-	      (reduce #'- (list card1 card2)
-		      :key (lambda (c) (position c *suit-order-jacks* :key #'value)))
+(deftest "compare-cards mit zwei Buben im Grandspiel" :category "Cards"
+	 :input-form (values #c hearts jack #c diamonds jack :grand)
+	 :test-fn #'compare-cards
+	 :output-form 1)
+
+;; Vorschlag für Makro:
+;; (deftests
+;;     ("text" (compare-cards #c diamonds jack #c hearts jack :null) 1)
+;;     ("text" (function-name input-form* ) output-form))
+
+(deftest "compare-cards mit zwei Buben im Nullspiel" :category "Cards"
+	 :input-form (values #c diamonds jack #c hearts jack :null)
+	 :test-fn #'compare-cards
+	 :output-form 1)
 
 (defun card-greater-p (card1 card2 game-variant)
   "Gibt zurück, ob card1 höher ist als card2"
