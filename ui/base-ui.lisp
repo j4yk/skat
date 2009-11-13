ac5a199f2caa391265856c4415aa4bae46c4b6d1
Jakob Reschke <jakob@resfarm.de>
2009-11-13 23:33:18 +0100

    generic functions für start und stop von UIs

--- ui/base-ui.lisp
+++ ui/base-ui.lisp
@@ -3,3 +3,12 @@
 (defclass base-ui ()
   ((kernel :accessor kernel :initarg :kernel))
   (:documentation "Basisklasse für alle Ausprägungen von Benutzerschnittstellen und Hauptschleifenobjekten."))
+
+(defgeneric start (ui &optional no-new-thread-p)
+  (:documentation "Startet die Benutzerschnittstelle.
+Gebärt dies in einer Implementierung normalerweise einen neuen Thread, kann der UI
+durch setzen von no-new-thread-p auf t signalisiert werden, dass sie davon absehen soll."))
+
+(defgeneric stop (ui)
+  (:documentation "Signalisiert einer UI, dass sie ihre Arbeit so bald wie möglich
+einstellen soll."))
