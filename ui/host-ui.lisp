31cb424f0a28d0cdfaa77397ff816fcdf7aa6739
Jakob Reschke <jakob@resfarm.de>
2009-11-16 18:51:49 +0100

    host-ui: Stoppmechanismus eingebaut
    und diverse Versuche fürs Multithreading, da das ja bisher
    immer abstürzt

--- ui/host-ui.lisp
+++ ui/host-ui.lisp
@@ -1,7 +1,9 @@
 (in-package skat-ui)
 
 (defclass host-ui (base-ui)
-  ()
+  ((stop-received :documentation "t, nachdem die Methode stop aufgerufen wurde")
+   (standard-output :documentation "Standard Output stream" :initarg :stdout)
+   (debug-stream :accessor debug-stream :initarg :debugstream))
   (:documentation "Unsichtbare UI, ist nur als logische Komponente eine Schnittstelle."))
 
 ;;; host-ui implementiert keine Handler, da bestenfalls auch keine Anfragen an eine
@@ -9,15 +11,40 @@
 ;;; Jedoch muss in dieser Komponente hier die Hauptschleife laufen.
 
 (defmethod main-loop ((ui host-ui))
-  "Die Hauptschleife für den Host"
-  (warn "Bis jetzt kein Stop implementiert!")
+  "Die Hauptschleife für den Host.
+Endet, wenn Slot stop-received initialisiert wird."
   (loop
-       (kernel:receive-requests (kernel ui))))
+     (sleep 0.5)
+     (kernel:receive-requests (kernel ui))
+     (when (slot-boundp ui 'stop-received)
+       (return))))
+
+(defun error-in-host-main-loop (condition ui)
+  "Gibt den Fehler aus und lässt die Schleife weiterlaufen (continue restart)"
+;  (format (debug-stream ui) "~a: ~a" (type-of condition) condition)
+  (continue condition))
+
+(defmethod start-main-loop ((ui host-ui))
+  "Startet die Hauptschleife für den Host."
+;;   (when (slot-boundp ui 'standard-output)
+;;     (setq *standard-output* (slot-value ui 'standard-output))
+;;     (format *standard-output* "*standard-output* im UI-Loop gebunden")
+;;     (format t "test")) ; für SLIME
+;;   (when (slot-boundp ui 'debug-stream)
+;;     (setq *debug-io* (slot-value ui 'debug-stream))
+;;     (format *debug-io* "*debug-io* im UI-Loop gebunden"))
+  (handler-bind ((error #'(lambda (&optional condition) (error-in-host-main-loop condition ui))))
+    (restart-case (main-loop ui)
+      (continue (condition)
+	(declare (ignorable condition)) (main-loop ui)))))
 
 (defmethod start ((ui host-ui) &optional no-new-thread-p)
   "Startet die Hauptschleife für den Host in einem neuen Thread."
-  (labels ((start-main-loop ()
-	     (main-loop ui)))
-    (if no-new-thread-p
-	(start-main-loop)
-	(sb-thread:make-thread #'start-main-loop :name "Host Main Loop Thread"))))
+  (slot-makunbound ui 'stop-received)
+  (if no-new-thread-p
+      (main-loop ui)
+      (sb-thread:make-thread #'(lambda () (main-loop ui)) :name "Host Main Loop Thread")))
+
+(defmethod stop ((ui host-ui))
+  "Hält die UI an, indem die Abbruchvariable gesetzt wird."
+  (setf (slot-value ui 'stop-received) t))
