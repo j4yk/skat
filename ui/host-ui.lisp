c31563ce24893bba97b27ff7e7809ec15765c664
Jakob Reschke <jakob@resfarm.de>
2009-11-16 23:38:37 +0100

    host-ui Funktionen etwas klarer benannt

--- ui/host-ui.lisp
+++ ui/host-ui.lisp
@@ -11,39 +11,28 @@
 ;;; Jedoch muss in dieser Komponente hier die Hauptschleife laufen.
 
 (defmethod main-loop ((ui host-ui))
-  "Die Hauptschleife für den Host.
+  "Führt die Hauptschleife für den Host aus.
 Endet, wenn Slot stop-received initialisiert wird."
   (loop
      (sleep 0.5)
-     (kernel:receive-requests (kernel ui))
+     (restart-case (kernel:receive-requests (kernel ui))
+       (continue-main-loop ()
+	 :report "setze die Hauptschleife der Host-UI fort"
+	 (values)))	 
      (when (slot-boundp ui 'stop-received)
        (return))))
 
-(defun error-in-host-main-loop (condition ui)
-  "Gibt den Fehler aus und lässt die Schleife weiterlaufen (continue restart)"
-;  (format (debug-stream ui) "~a: ~a" (type-of condition) condition)
-  (continue condition))
-
-(defmethod start-main-loop ((ui host-ui))
-  "Startet die Hauptschleife für den Host."
-;;   (when (slot-boundp ui 'standard-output)
-;;     (setq *standard-output* (slot-value ui 'standard-output))
-;;     (format *standard-output* "*standard-output* im UI-Loop gebunden")
-;;     (format t "test")) ; für SLIME
-;;   (when (slot-boundp ui 'debug-stream)
-;;     (setq *debug-io* (slot-value ui 'debug-stream))
-;;     (format *debug-io* "*debug-io* im UI-Loop gebunden"))
-  (handler-bind ((error #'(lambda (&optional condition) (error-in-host-main-loop condition ui))))
-    (restart-case (main-loop ui)
-      (continue (condition)
-	(declare (ignorable condition)) (main-loop ui)))))
+(defmethod main-loop-in-other-thread ((ui host-ui))
+  "Startet die Hauptschleife für den Host. Soll aus einem anderen Thread heraus gestartet werden."
+  (handler-bind ((error #'(lambda () (invoke-restart 'continue-main-loop))))
+    (main-loop ui)))
 
 (defmethod start ((ui host-ui) &optional no-new-thread-p)
   "Startet die Hauptschleife für den Host in einem neuen Thread."
   (slot-makunbound ui 'stop-received)
   (if no-new-thread-p
       (main-loop ui)
-      (sb-thread:make-thread #'(lambda () (main-loop ui)) :name "Host Main Loop Thread")))
+      (sb-thread:make-thread #'(lambda () (main-loop-in-other-thread ui)) :name "Host Main Loop Thread")))
 
 (defmethod stop ((ui host-ui))
   "Hält die UI an, indem die Abbruchvariable gesetzt wird."
