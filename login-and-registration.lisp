c98eafae80e5e1e3bd14f831d5b54b1541fc2e67
Jakob Reschke <jakob@resfarm.de>
2009-12-12 20:01:55 +0100

    slots werden nun alle in *struct-slots* buchgeführt
    keine Trennung mehr nach login-data und registration-data
    ist auch nicht nötig, da sowieso nach Structname unterschieden wird
    
    mit slots-of-class kann die Liste abgefragt werden

--- login-and-registration.lisp
+++ login-and-registration.lisp
@@ -4,7 +4,15 @@
 
 (defstruct login-data "Basisklasse für alle möglichen login-data structs.")
 
-(defvar *login-data-slots* nil "Assoc-Liste mit den Slot-Definitionen")
+(defvar *struct-slots* nil "Assoc-Liste mit den Slot-Definitionen")
+
+(defun slots-of-class (classname)
+  "Gibt die Liste der Slot-Definitionen für diesen Struct-Typ zurück,
+wenn er mit define-struct-with-slot-dict definiert wurde.
+Das trifft auf mit define-login-data und define-registration-data
+definierte Structs."
+  (if (assoc classname *struct-slots*)
+      (cdr (assoc classname *struct-slots*))))
 
 (defstruct (example-login-data (:include login-data)) "Beispiel für eine login-data Struktur."
 	   (username nil :type string) (password nil :type string))
@@ -17,7 +25,7 @@
     (:en-gb (username "User name" "Description of the field User Name")
      (password "Password" "Description of the field Password"))))
 
-(defmacro define-struct-with-slot-dict (name-and-options slot-dict &rest slot-descriptions)
+(defmacro define-struct-with-slot-dict (name-and-options &rest slot-descriptions)
   "Definiert einen neuen Struct und pusht die Slots in das slot-dict."
   (let ((pure-slots (labels ((shrink (slots)
 			       (if (listp (car slots))
@@ -29,23 +37,21 @@
      (defstruct ,name-and-options ,@slot-descriptions) ; defstruct
      (push '(,(if (listp name-and-options)
 		  (car name-and-options)
-		  name-and-options) ,@pure-slots) ,slot-dict)))) ; und Slots eintragen
+		  name-and-options) ,@pure-slots) *struct-slots*)))) ; und Slots eintragen
 
 (defmacro define-login-data (name &rest slot-descriptions)
   "Definiert eine neue Login-Datenstruktur"
-  `(define-struct-with-slot-dict (,name (:include login-data))
-       *login-data-slots* ,@slot-descriptions))
+  `(define-struct-with-slot-dict (,name (:include login-data)) ,@slot-descriptions))
 
 ;;; Registration-Data
 
 (defstruct registration-data "Basisklasse für alle möglichen registration-data structs.")
 
-(defvar *registration-data-slots* nil "Assoc-Liste mit den Slot-Definitionen")
-
 (defmacro define-registration-data (name &rest slot-descriptions)
   "Definiert eine neue Registrierungs-Datenstruktur."
-  `(define-struct-with-slot-dict (,name (:include registration-data))
-       *registration-data-slots* ,@slot-descriptions))
+  `(define-struct-with-slot-dict (,name (:include registration-data)) ,@slot-descriptions))
+
+;;; Tests
 
 (deftest "define-login-data"
     :category "login-and-registration"
@@ -54,7 +60,7 @@
 		  (macroexpand '(define-login-data test "Teststruct" (test nil :type t)))
 		  '(progn
 		    (defstruct (test (:include login-data)) "Teststruct" (test nil :type t))
-		    (push '(test (test nil :type t)) *login-data-slots*)))))
+		    (push '(test (test nil :type t)) *struct-slots*)))))
 
 (deftest "define-registration-data"
     :category "login-and-registration"
@@ -63,4 +69,11 @@
 		  (macroexpand '(define-registration-data test "Teststruct" (test nil :type t)))
 		  '(progn
 		    (defstruct (test (:include registration-data)) "Teststruct" (test nil :type t))
-		    (push '(test (test nil :type t)) *registration-data-slots*)))))
+		    (push '(test (test nil :type t)) *struct-slots*)))))
+
+(deftest "slot-dict"
+    :category "login-and-registration"
+    :test-fn #'(lambda ()
+		 (define-struct-with-slot-dict ___test a (b 5) (c nil :type t))
+		 (slots-of-class '___test))
+    :output-form '(a (b 5) (c nil :type t)))
