590fa54faa8f3304a9123ab9f4802a10af38d084
Jakob Reschke <jakob@resfarm.de>
2009-12-21 23:08:52 +0100

    Stub-UI lässt sich jetzt mit ui::*stub-ui-verbosity* knebeln

--- ui/stub-ui.lisp
+++ ui/stub-ui.lisp
@@ -4,6 +4,13 @@
   ((received-requests :accessor received-requests :initform nil))
   (:documentation "Schritt- für Schritt UI, kann gar nichts."))
 
+(defvar *stub-ui-verbosity* 1 "Regelt die Gesprächigkeit aller stub-uis")
+
+(defmacro verbose (min-level &body body)
+  "Führt body nur aus, wenn *stub-ui-verbosity* mindestens min-level ist"
+  `(when (>= *stub-ui-verbosity* ,min-level)
+     ,@body))
+
 (defmethod print-object ((ui stub-ui) stream)
   (print-unreadable-object (ui stream :type t)
     (princ "of " stream)
@@ -28,7 +35,7 @@
 
 (defmethod main-loop-step ((ui stub-ui))
   "Schiebt den Kernel an, seine Requests abzuholen."
-  (stub-ui-msg ui "doing step.")
+  (verbose 1 (stub-ui-msg ui "doing step."))
   (kernel:receive-requests (kernel ui)))
 
 (defmethod just-one-step ((ui stub-ui))
@@ -36,14 +43,14 @@
 
 (defmethod send-request-to-kernel ((ui stub-ui) request-name &rest arguments)
   "Sendet eine Anfrage an den Kernel, der sie in den meisten Fällen dann verschicken wird."
-  (stub-ui-msg ui "sending ~s ~s to kernel" request-name arguments)
+  (verbose 1 (stub-ui-msg ui "sending ~s ~s to kernel" request-name arguments))
   (apply #'kernel:call-handler-fn (kernel ui) ui request-name arguments))
 
 (defmethod start ((ui stub-ui) &optional to-be-ignored)
   "Tut eigentlich nichts...
 to-be-ignored, da host-ui bspw. einen zweiten Parameter nimmt"
   (declare (ignore to-be-ignored))
-  (stub-ui-msg ui "started.")
+  (verbose 1 (stub-ui-msg ui "started."))
   (values))
 
 (define-condition stub-ui-request-arrived ()
@@ -52,7 +59,7 @@ to-be-ignored, da host-ui bspw. einen zweiten Parameter nimmt"
   (:documentation "Condition für den Fall, dass eine Stub-UI etwas empfangen hat."))
 
 (define-all-requests-handler (stub-ui request-call)
-  (stub-ui-msg ui "received ~s ~s from ~s" (car request-call) (cdr request-call) sender)
+  (verbose 1 (stub-ui-msg ui "received ~s ~s from ~s" (car request-call) (cdr request-call) sender))
   (setf (received-requests ui) (nconc (received-requests ui) (list request-call))) ; die Anfrage notieren
   (restart-case (signal 'stub-ui-request-arrived :ui ui :request-call request-call)
-    (continue () t)))			
+    (continue () t)))
