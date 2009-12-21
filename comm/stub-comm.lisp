eb85f37d1579b9e020438afd792f6ee52a5a4ba5
Jakob Reschke <jakob@resfarm.de>
2009-12-21 16:01:09 +0100

    *stub-comm-verbosity* eingeführt, damit nicht immer alles gedruckt wird
    2 druckt alles
    1 druckt push-requests nicht mit
    0 druckt nichts

--- comm/stub-comm.lisp
+++ comm/stub-comm.lisp
@@ -4,6 +4,14 @@
   ((id :accessor id :initform (gensym "STUB-COMM") :initarg :id)
    (address-compare-function :initform #'eq)))
 
+(defvar *stub-comm-verbosity* 2 "Gibt an, wie viele Nachrichten stub-comms drucken.
+0 = keine, 1 = alles außer push-request, 2 = alles")
+
+(defmacro verbose (min-level &body body)
+  "Führt body nur aus, wenn *stub-comm-verbosity* mindestens min-level ist"
+  `(when (>= *stub-comm-verbosity* ,min-level)
+     ,@body))
+
 (kern:define-registration-data stub-registration-data (host-comm nil :type stub-comm))
 
 (defmethod host-address ((comm stub-comm) (data stub-registration-data))
@@ -21,12 +29,12 @@
   stub-comm)
 
 (defmethod start ((comm stub-comm))
-  (stub-comm-msg comm "started.")
+  (verbose 1 (stub-comm-msg comm "started."))
   (push-request comm comm 'login-parameters (list nil))
   (values))
 
 (defmethod login ((comm stub-comm) data)
-  (stub-comm-msg comm "logged in.")
+  (verbose 1 (stub-comm-msg comm "logged in."))
   (push-request comm comm 'own-address (list (address comm)))
   (push-request comm comm 'registration-struct '(stub-registration-data))
   (values))
@@ -36,12 +44,12 @@
 
 (defmethod push-request :after ((comm stub-comm) sender request-name request-args)
   "Schreibt auf, was gepusht wurde"
-  (stub-comm-msg comm "pushed ~s ~s from ~s" request-name request-args sender))
+  (verbose 2 (stub-comm-msg comm "pushed ~s ~s from ~s" request-name request-args sender)))
 
 (defmethod get-request :around ((comm stub-comm))
   "Schreibt auf, was gepoppt wurde."
   (multiple-value-bind (request-name sender request-args) (call-next-method)
-    (stub-comm-msg comm "popped ~s ~s from ~s" request-name request-args sender)
+    (verbose 1 (stub-comm-msg comm "popped ~s ~s from ~s" request-name request-args sender))
     (values request-name sender request-args)))
 
 (define-condition stub-communication-send ()
@@ -52,7 +60,7 @@
   (:documentation "Condition zum automatisierten Weiterverarbeiten einer Sendung."))
 
 (defmethod send ((comm stub-comm) address request-name &rest request-args)
-  (stub-comm-msg comm "sending ~s ~s to ~s" request-name request-args address)
+  (verbose 1 (stub-comm-msg comm "sending ~s ~s to ~s" request-name request-args address))
   ;; lasse wen will hier einhängen, damit noch was automatisiert werden kann
   (restart-case (signal 'stub-communication-send :request request-name :args request-args
 			:sender comm :address address)
