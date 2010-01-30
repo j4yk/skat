6a60cdc5cd8ddcb140776f68dd7f7cc3aaf824d7
Jakob Reschke <jakres@gmail.com>
2010-01-30 01:54:55 +0100

    erste Makros zum Testen eingeführt
    with-skat-window, with-agar-enabled-skat-window usw.

--- ui/opengl-ui/test-utils.lisp
+++ ui/opengl-ui/test-utils.lisp
@@ -1,3 +1,35 @@
 (in-package skat-opengl-ui)
 
 ;; Hier sollen Makros rein, die das Testen von Modulen usw. erleichtern.
+
+(defmacro with-skat-window (&body body)
+  "Initialisiert SDL und erstellt das Skatfenster."
+  `(sdl:with-init ()
+     (skat-window)
+     ,@body))
+
+(defmacro standard-with-events (&body event-handlers)
+  "Definiert das Quit-Event und startet die Schleife"
+  `(sdl:with-events (:poll sdl-event)
+     (:quit-event () t)
+     ,@event-handlers))
+
+(defmacro with-agar-enabled-skat-window (&body body)
+  "Initialisiert SDL, das Skatfenster und Agar"
+  `(with-skat-window
+     (agar:with-agar-core ("Skat")
+       (agar:with-sdl-video ((sdl:fp sdl:*default-display*) :opengl) ; AG initialisieren und anbinden
+	 ,@body))))
+
+(defmacro test-module (module-class &rest initargs)
+  (let ((module (gensym "module")))
+    `(with-agar-enabled-skat-window
+       (let ((,module (make-instance ,module-class ,@initargs)))
+	 (standard-with-events
+	   ((:mouse-button-down-event ()
+				      (handle-event ,module sdl-event))
+	    (:mouse-button-up-event ()
+				    (handle-event ,module sdl-event))
+	    (:idle ()
+		   (agar:render
+		     (draw ,module)))))))))
