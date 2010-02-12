52a30580bec9a5270207e4396214611f0f68c7e0
Jakob Reschke <jakres@gmail.com>
2010-02-13 00:18:23 +0100

    outsourced the actual login-window into an extra class
    this is just not to clutter the module
    The layout is created with ag:with-widgets layout language.

--- ui/opengl-ui/login-and-register.lisp
+++ ui/opengl-ui/login-and-register.lisp
@@ -8,14 +8,49 @@
    (register-window :accessor register-window))
   (:documentation "Modul für die Einwähl- und Registrier-Prozedur"))
 
+(defclass agar-window ()
+  ((widgets :accessor widgets)
+   (window :accessor window)))
+
+(defmethod show ((agar-window agar-window))
+  "Calls AG_WindowShow on the Agar window"
+  (ag:window-show (window agar-window)))
+
+(defmethod draw ((agar-window agar-window))
+  (ag:window-draw (window agar-window)))
+
+(defclass login-window (agar-window)
+  ())
+
+(defmethod initialize-instance :after ((login-window login-window) &key)
+  "Creates the Agar Window"
+  (let ((win (agar:window-new :modal :noclose)))
+    (ag:window-set-caption win "Beim XMPP-Server einlogen")
+    (ag:with-widgets (win
+		      (ag:vbox box1 ()
+			       (ag:hbox username-box ()
+					(ag:textbox username-textbox :label-text "Benutzername:" :flags '(:abandon-focus)))
+			       (ag:hbox server-box ()
+					(ag:textbox server-textbox :label-text "Serveradresse:" :flags '(:abandon-focus)))
+			       (ag:hbox domain-box ()
+					(ag:textbox  domain-textbox :label-text "Serverdomäne (optional):" :flags '(:abandon-focus)))
+			       (ag:hbox password-box ()
+					(ag:textbox password-textbox :label-text "Passwort:" :flags '(:password :abandon-focus)))
+			       (ag:hbox resource-box ()
+					(ag:textbox resource-textbox :label-text "Standard (optional):" :init-text "skat"
+						     :flags '(:abandon-focus)))))
+      (with-slots (widgets window) login-window
+	(setf window win
+	      widgets (list (cons :username username-textbox)
+			    (cons :server server-textbox)
+			    (cons :domain domain-textbox)
+			    (cons :password password-textbox)
+			    (cons :resource resource-textbox)))))))
+
 (defun init-login-window (module)
   "Erstelle das Login-Daten-Fenster und zeige es an."
-  (let ((window (agar:window-new :modal :noclose)))
-    (setf (login-window module) window)
-    (agar:window-set-caption window "Login")
-    (agar:label-new-string window "Hallöle")
-    (agar::text-msg :info "bar")
-    (agar:window-show window)))
+  (setf (login-window module) (make-instance 'login-window))
+  (show (login-window module)))
 
 (defmethod initialize-instance :after ((module login-and-register-module)
 				       &rest initargs
@@ -28,8 +63,8 @@
   "Zeichne das Logindaten- oder Registrierungsdaten-Fenster"
   ;; zeichne deine Fenster
   (case (state module)
-    (enter-login (agar:window-draw (login-window module)))))
+    (enter-login (draw (login-window module)))))
 
 (defmethod handle-event ((module login-and-register-module) event)
   ;; handle event
-  (agar:process-event event))
+  (ag:process-event event))
