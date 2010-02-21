2286f23f3e6228a520631c2ee96962da1272da0c
Jakob Reschke <jakres@gmail.com>
2010-02-21 22:59:38 +0100

    cards module now uses the GNOME cards images for textures

--- ui/opengl-ui/cards.lisp
+++ ui/opengl-ui/cards.lisp
@@ -22,17 +22,24 @@
   (map nil
        #'(lambda (name texture)
 	   (add-texture module name texture))
-       (list :diamonds7 :diamonds8 :diamonds9 :diamondsQ
-	     :diamondsK :diamonds10 :diamondsA :diamondsJ)
-       (loop for val from 0.8 downto 0.1 by 0.1
-	  collect (create-solid-filled-texture 1 val val))))
+       (list :d7 :d8 :d9 :dq :dk :d10 :da :dj
+	     :h7 :h8 :h9 :hq :hk :h10 :ha :hj
+	     :s7 :s8 :s9 :sq :sk :s10 :sa :sj
+	     :c7 :c8 :c9 :cq :ck :c10 :ca :cj)
+       (let ((*default-pathname-defaults* (merge-pathnames "resources/cards/")))
+	 (loop for suit in (list "diamond" "heart" "spade" "club")
+	    append (loop for rank in (list "7" "8" "9" "10" "queen" "king" "1" "jack")
+		      collect (sdl-surface-to-gl-texture
+			       (sdl:load-image
+				(merge-pathnames
+				 (concatenate 'string rank "_" suit ".png")))))))))
 
 (defmethod initialize-instance :after ((module cards) &key)
   (load-textures module))
 
 (defstruct card texture selection-name)
 
-(defun own-cards-selname (nthcard)
+(defun own-card-selname (nthcard)
   (let ((own-cards-offset 1000))
     (+ own-cards-offset nthcard)))
 
@@ -45,20 +52,14 @@
   (gl:with-pushed-matrix
     (let ((f (/ 1 3)))
       (gl:scale f f f))
-    (with-pushed-selname selection-name
+    (with-selname selection-name
       ;; Fläche
       (gl:with-primitives :polygon
 	(gl:tex-coord 0 1) (gl:vertex (/ -6 2) (/ -9 2)) ; unten links
 	(gl:tex-coord 1 1) (gl:vertex (/  6 2) (/ -9 2)) ; unten rechts
 	(gl:tex-coord 1 0) (gl:vertex (/  6 2) (/  9 2)) ; oben rechts
-	(gl:tex-coord 0 0) (gl:vertex (/ -6 2) (/  9 2))) ; oben links
-      ;; Rahmen
-      (gl:color 0 0 0)
-      (gl:with-primitives :line-loop
-	(gl:vertex (/ -6 2) (/ -9 2)) ; unten links
-	(gl:vertex (/  6 2) (/ -9 2)) ; unten rechts
-	(gl:vertex (/  6 2) (/  9 2)) ; oben rechts
-	(gl:vertex (/ -6 2) (/  9 2)))))) ; oben links
+	(gl:tex-coord 0 0) (gl:vertex (/ -6 2) (/  9 2)))))) ; oben links
+
 
 (defun draw-hand (module cards)
   "Zeichnet eine aufgefaltete Hand von Karten"
@@ -85,20 +86,26 @@
   "Zeichnet die Karten"
   (gl:matrix-mode :modelview)
   (gl:enable :texture-2d)
+;  (gl:enable :blend)
+;  (gl:blend-func :src-alpha :one-minus-src-alpha)
+  (gl:enable :alpha-test)
+  (gl:alpha-func :greater 0.1)
   (gl:load-identity)
   (gl:translate 0 0 -10)
   (gl:color 1 1 1)
   (gl:with-pushed-matrix
-					;     (gl:rotate -20 1 0 0)		; 45° nach vorn geneigt
     (gl:translate 0 -2 0)
-    (draw-hand module (list (make-card :texture :diamonds7)
-			    (make-card :texture :diamonds8)
-			    (make-card :texture :diamonds9)
-			    (make-card :texture :diamondsQ)
-			    (make-card :texture :diamondsK)
-			    (make-card :texture :diamonds10)
-			    (make-card :texture :diamondsA)
-			    (make-card :texture :diamondsJ)))))
+    (draw-hand module (list (make-card :texture :d7)
+			    (make-card :texture :d8)
+			    (make-card :texture :d9)
+			    (make-card :texture :dq)
+			    (make-card :texture :dk)
+			    (make-card :texture :d10)
+			    (make-card :texture :da)
+			    (make-card :texture :dj))))
+;  (gl:disable :blend)
+  (gl:disable :alpha-test)
+  (gl:disable :texture-2d))
 
 (defmethod handle-event ((module cards) event)
   (case-event event
