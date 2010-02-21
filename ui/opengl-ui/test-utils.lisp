d7fae48be5a37191245b221487462cca69fbe7f3
Jakob Reschke <jakres@gmail.com>
2010-02-21 23:12:12 +0100

    found a better way for testing than the with- macros
    init-window opens the SDL window
    init-agar makes Agar use it
    setup-video does both
    kill-video... well kills both, the Window will disappear
    
    test-module is adapted to make use of the new concept

--- ui/opengl-ui/test-utils.lisp
+++ ui/opengl-ui/test-utils.lisp
@@ -2,37 +2,53 @@
 
 ;; Hier sollen Makros rein, die das Testen von Modulen usw. erleichtern.
 
-(defmacro with-skat-window (&body body)
-  "Initialisiert SDL und erstellt das Skatfenster."
-  `(sdl:with-init ()
-     (skat-window)
-     (init-gl 640 480)
-     ,@body))
+;; (defmacro with-skat-window (&body body)
+;;   "Initialisiert SDL und erstellt das Skatfenster."
+;;   `(sdl:with-init (sdl:sdl-init-video)
+;;      (skat-window)
+;;      (init-gl 640 480)
+;;      ,@body))
 
-(defmacro standard-with-events (&body event-handlers)
-  "Definiert das Quit-Event und startet die Schleife"
-  `(sdl:with-events (:poll sdl-event)
-     (:quit-event () t)
-     ,@event-handlers))
+;; (defmacro standard-with-events (&body event-handlers)
+;;   "Definiert das Quit-Event und startet die Schleife"
+;;   `(sdl:with-events (:poll sdl-event)
+;;      (:quit-event () t)
+;;      ,@event-handlers))
 
-(defmacro with-agar-enabled-skat-window (&body body)
-  "Initialisiert SDL, das Skatfenster und Agar"
-  `(with-skat-window
-     #+agar
-     (agar:with-agar-core ("Skat")
-       (agar:with-sdl-video ((sdl:fp sdl:*default-display*) :opengl) ; AG initialisieren und anbinden
-	 ,@body))
-     #-agar
-     (progn
-       ,@body)))
+;; (defmacro with-agar-enabled-skat-window (&body body)
+;;   "Initialisiert SDL, das Skatfenster und Agar"
+;;   `(with-skat-window
+;;      #+agar
+;;      (agar:with-agar-core ("Skat")
+;;        (agar:with-sdl-video ((sdl:fp sdl:*default-display*) :opengl :overlay) ; AG initialisieren und anbinden
+;; 	 ,@body))
+;;      #-agar
+;;      (progn
+;;        ,@body)))
 
-(defmacro test-module (module-class &rest initargs)
-  (assert (find-class (cadr module-class))) ; quoted!
-  (let ((module (gensym "module")))
-    `(with-agar-enabled-skat-window
-       (sdl:show-cursor :enable)
-       (let ((,module (make-instance ,module-class ,@initargs))
-	     (ui (make-instance 'opengl-ui)))
-	 (defparameter *module* ,module)
-	 (push ,module (modules ui))
-	 (standard-main-loop ui)))))
+(defun init-window (&optional sdl-flags)
+  (sdl:init-sdl :flags sdl-flags)
+  (defparameter *sdl-screen* (skat-window)))
+
+(defun init-agar (&rest flags)
+  (apply #'ag::init-video-sdl (sdl:fp *sdl-screen*) flags))
+
+(defun kill-video ()
+  (ag::destroy-video)
+  (sdl:quit-sdl)
+  (defparameter *sdl-screen* nil)
+  (values))
+
+(defun setup-video ()
+  (init-window)
+  (init-agar))
+
+(defun test-module (module-class &rest initargs)
+  (assert (find-class module-class))
+  (defparameter *module* (apply #'make-instance module-class initargs))
+  (unless (boundp 'ui)
+    (defparameter ui (make-instance 'opengl-ui))
+    (push (make-instance 'agar) (modules ui)))
+  (unless (member (find-class module-class) (modules ui) :key #'class-of)
+    (push *module* (modules ui)))
+  (standard-main-loop ui))
