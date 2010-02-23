048b6e5646a55f96a19802686014590263e05018
Jakob Reschke <jakres@gmail.com>
2010-02-23 20:51:58 +0100

    fixed: forgot to initialize Agar in the test functions

--- ui/opengl-ui/test-utils.lisp
+++ ui/opengl-ui/test-utils.lisp
@@ -1,57 +1,42 @@
 (in-package skat-opengl-ui)
 
-;; Hier sollen Makros rein, die das Testen von Modulen usw. erleichtern.
+;; Hier sollen die Sachen rein, die das Testen von Modulen usw. erleichtern.
 
-;; (defmacro with-skat-window (&body body)
-;;   "Initialisiert SDL und erstellt das Skatfenster."
-;;   `(sdl:with-init (sdl:sdl-init-video)
-;;      (skat-window)
-;;      (init-gl 640 480)
-;;      ,@body))
+(defparameter *sdl-screen* nil)
 
-;; (defmacro standard-with-events (&body event-handlers)
-;;   "Definiert das Quit-Event und startet die Schleife"
-;;   `(sdl:with-events (:poll sdl-event)
-;;      (:quit-event () t)
-;;      ,@event-handlers))
-
-;; (defmacro with-agar-enabled-skat-window (&body body)
-;;   "Initialisiert SDL, das Skatfenster und Agar"
-;;   `(with-skat-window
-;;      #+agar
-;;      (agar:with-agar-core ("Skat")
-;;        (agar:with-sdl-video ((sdl:fp sdl:*default-display*) :opengl :overlay) ; AG initialisieren und anbinden
-;; 	 ,@body))
-;;      #-agar
-;;      (progn
-;;        ,@body)))
-
-(defun init-window (&optional sdl-flags)
+(defun init-window (&rest sdl-flags)
   (sdl:init-sdl :flags sdl-flags :video t)
   #+windows (sdl:init-image :png)
   (defparameter *sdl-screen* (skat-window)))
 
 (defun init-agar (&rest flags)
+  (assert *sdl-screen*)
+  #+agar
+  (ag::init-core "skat" 0)
   #+agar
   (apply #'ag::init-video-sdl (sdl:fp *sdl-screen*) flags))
 
 (defun kill-video ()
   #+agar
   (ag::destroy-video)
+  (sdl:close-audio)
   (sdl:quit-sdl)
-  (defparameter *sdl-screen* nil)
+  (setq *sdl-screen* nil)
   (values))
 
 (defun setup-video ()
   (init-window)
-  (init-agar))
+  (init-agar :overlay))
+
+(defparameter *ui* nil)
+(defparameter *module* nil)
 
 (defun test-module (module-class &rest initargs)
   (assert (find-class module-class))
-  (defparameter *module* (apply #'make-instance module-class initargs))
-  (unless (boundp 'ui)
-    (defparameter ui (make-instance 'opengl-ui))
-    (push (make-instance 'agar) (modules ui)))
-  (unless (member (find-class module-class) (modules ui) :key #'class-of)
-    (push *module* (modules ui)))
-  (standard-main-loop ui))
+  (setq *module* (apply #'make-instance module-class initargs))
+  (unless *ui*
+    (setq *ui* (make-instance 'opengl-ui))
+    (push (make-instance 'agar) (modules *ui*)))
+  (unless (member (find-class module-class) (modules *ui*) :key #'class-of)
+    (push *module* (modules *ui*)))
+  (standard-main-loop *ui*))
