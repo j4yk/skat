d0b0a5d04ff6523290a993d6a514c243b487bffb
Jakob Reschke <jakres@gmail.com>
2010-02-26 23:28:37 +0100

    extended the agar-test module: free the windows and have a label each

--- ui/opengl-ui/agar-test.lisp
+++ ui/opengl-ui/agar-test.lisp
@@ -4,4 +4,11 @@
   ((wins :accessor wins :initform nil)))
 
 (defmethod initialize-instance :after ((mod agar-test) &key)
-  (dotimes (n 5) (let ((w (ag:window-new))) (push w (wins mod)) (ag:window-show w))))
+  (dotimes (n 5)
+    (let ((w (ag:window-new)))
+      (push w (wins mod))
+      (ag::label-new-string w (format nil "Window ~s" n))
+      (ag:window-show w)
+      (trivial-garbage:finalize
+       mod
+       #'(lambda () (ag::destroy-object w))))))
