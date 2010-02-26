48f53ca870e7be8ff18f31699e2fbeb2e8eb8379
Jakob Reschke <jakres@gmail.com>
2010-02-26 23:21:57 +0100

    fixed: Agar's timeouts weren't processed
    ag:process-timeouts is now called in the draw method of Agar module
    when there are timeouts queued

--- ui/opengl-ui/agar.lisp
+++ ui/opengl-ui/agar.lisp
@@ -9,7 +9,10 @@
   (ag:process-event sdl-event))
 
 (defmethod draw ((module agar))
-  "Draw Agar's windows"
+  "Draw Agar's windows and process Timeouts"
+  #+agar
+  (when (ag::timeouts-queued-p)
+    (ag::process-timeouts (sdl:system-ticks)))
   #+agar
   (let ((winlist (ag::tailqueue-to-list (ag::windows ag::*view*) #'ag::next-window)))
     (dolist (win winlist)
