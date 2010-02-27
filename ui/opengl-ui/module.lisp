edfa4f4f96ae32acb068e395c620766436b4bc0c
Jakob Reschke <jakres@gmail.com>
2010-02-27 17:27:35 +0100

    new generic method: (cleanup module)
    should be used to hide Agar windows and free allocated memory

--- ui/opengl-ui/module.lisp
+++ ui/opengl-ui/module.lisp
@@ -8,14 +8,20 @@
 ;; Module sollten (initialize-instance :after) implementieren,
 ;; um Aufbauarbeiten (Agar: Fenster erstellen etc.) durchzuführen
 
+(defgeneric cleanup (module)
+  (:documentation "Lässt ein Modul von ihm reservierten Speicher freigeben, Fenster schließen etc."))
+
 (defgeneric draw (module)
   (:documentation "Lässt ein Modul seine Grafiken zeichnen"))
 
 (defgeneric handle-event (module event)
   (:documentation "Lässt ein Modul das SDL_Event verarbeiten"))
 
-(defmethod handle-event ((module module) event)
-  "No-op as fallback")
+(defmethod cleanup ((module module))
+  "Default: No-op.")
 
 (defmethod draw ((module module))
+  "No-op as fallback")
+
+(defmethod handle-event ((module module) event)
   "No-op as fallback")
