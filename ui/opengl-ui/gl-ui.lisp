2083bc1b184705e4aa5f18b21ff61d915a8a87a6
Jakob Reschke <jakres@gmail.com>
2010-03-04 23:47:12 +0100

    split up remove-module into remove-module, cleanup-and-remove-module does now the oridoes now what remove-module used to do

--- ui/opengl-ui/gl-ui.lisp
+++ ui/opengl-ui/gl-ui.lisp
@@ -14,8 +14,11 @@
   (push module (modules ui)))
 
 (defun remove-module (module ui)
-  (cleanup module)
   (setf (modules ui) (delete module (modules ui))))
+
+(defun cleanup-and-remove-module (module ui)
+  (cleanup module)
+  (remove-module module ui))
 
 (defmethod ui:start ((ui opengl-ui) &optional no-new-thread-p)
   "Startet die OpenGL-Benutzerschnittstelle.
