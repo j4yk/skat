26b1123fcd062c924a8555a5980e54e3830adb62
Jakob Reschke <jakres@gmail.com>
2010-03-05 23:55:38 +0100

    further implemented login-and-register behaviour
    reacts now on query-registration, registration-accepted and
    registration-denied functions
    
    agar-window widgets are now contained in slots of the window-class
    rather than stored in that assoc-list --> slot-names can be verified
    by the compiler as opposed to assoc-ing which is all done at run-time
    
    there are some tries in to fix the status labels not being visible
    because the windows are too small, but they do not yet really work

--- ui/opengl-ui/login-and-register.lisp
+++ ui/opengl-ui/login-and-register.lisp
@@ -23,9 +23,6 @@
 (defmethod hide ((agar-window agar-window))
   (ag:hide-window (window agar-window)))
 
-(defclass login-window (agar-window)
-  nil)
-
 (defmacro callback (name return-type args &body body)
   "Defines and returns the pointer to a callback function"
   `(progn
@@ -40,53 +37,125 @@
 	`(callback ,callback-name :void ((event ag:event))
 	   (funcall ,function-name event)))))
 
+(defun ensure-detached (object parent)
+  (when (pointer-eq (ag:parent-object object) parent)
+    (ag:detach-object object)))
+
+(defun ensure-attached (object parent)
+  (declare (optimize debug))
+  (unless (pointer-eq (ag:parent-object object) parent)
+    (ag:attach-object parent object)))
+
+(defclass login-window (agar-window)
+  ((username-textbox)
+   (server-hostname-textbox)
+   (server-domain-textbox)
+   (resource-textbox)
+   (password-textbox)
+   (login-button)
+   (wait-label)))
+
 (defmethod initialize-instance :after ((login-window login-window) &key)
   "Creates the Agar Window"
-  (let ((win (agar:window-new-named "login-window" :modal :noclose)))
+  (let ((win (agar:window-new :modal :noclose)))
     (ag:window-set-caption win "Beim XMPP-Server einlogen")
     (ag:with-widgets (win
-		      (ag:textbox username-textbox :label-text "Benutzername: ")
-		      (ag:textbox hostname-textbox :label-text "Serveradresse: ")
-		      (ag:textbox domain-textbox :label-text "Serverdomäne (optional): ")
-		      (ag:textbox password-textbox :label-text "Passwort: " :flags '(:password))
-		      (ag:textbox resource-textbox :label-text "Standard (optional): " :init-text "skat")
+		      (ag:textbox username-tb :label-text "Benutzername: ")
+		      (ag:textbox hostname-tb :label-text "Serveradresse: ")
+		      (ag:textbox domain-tb :label-text "Serverdomäne (optional): ")
+		      (ag:textbox password-tb :label-text "Passwort: " :flags '(:password))
+		      (ag:textbox resource-tb :label-text "Standard (optional): " :init-text "skat")
 		      (ag:button login-btn "Login"))
       (ag:set-event login-btn "button-pushed" (event-handler #'(lambda (event)
 								 (declare (ignore event))
 								 (send-login-data (module login-window)))) "")
-      (with-slots (widgets window widget-instances) login-window
+      (with-slots (window username-textbox server-hostname-textbox server-domain-textbox
+			  resource-textbox password-textbox login-button) login-window
 	(setf window win
-	      widgets (list (cons :username username-textbox)
-			    (cons :hostname hostname-textbox)
-			    (cons :domain domain-textbox)
-			    (cons :password password-textbox)
-			    (cons :resource resource-textbox)
-			    (cons :login-btn login-btn)))))))
-
-(defclass register-window (agar-window)
-  nil)
-
-(defmethod initialize-instance :after ((register-window register-window) &key)
-  "Initilializes the register window by creating the widgets etc."
-  (let ((win (ag:window-new-named "register-window" :modal :noclose :nominimize)))
-    (ag:with-widgets (win
-		      (ag:textbox host-jid-textbox :label-text "Jabber-ID des Hostes: ")
-		      (ag:button register-btn "Registrierung anfragen"))
-      (ag:set-event register-btn "button-pushed"
-		    (event-handler #'(lambda (event)
-				       (declare (ignore event))
-				       (send-registration-data (module register-window))))
-		    "")
-      (with-slots (widgets window) register-window
-	(setf window win
-	      widgets (list (cons :host-jid host-jid-textbox)))))))
+	      username-textbox username-tb
+	      server-hostname-textbox hostname-tb
+	      server-domain-textbox domain-tb
+	      resource-textbox resource-tb
+	      password-textbox password-tb
+	      login-button login-btn)))))
 
 (defun init-login-window (module)
   "Erstelle das Login-Daten-Fenster und zeige es an."
   (setf (login-window module) (make-instance 'login-window :module module)))
 
+(defmethod send-login-data ((module login-and-register))
+  (with-slots (window username-textbox server-hostname-textbox server-domain-textbox
+		      resource-textbox password-textbox wait-label login-button)
+      (login-window module)
+    (call-kernel-handler (ui module) 'login-data
+			 (comm::make-xmpp-login-data
+			  :username (ag:text username-textbox)
+			  :hostname (ag:text server-hostname-textbox)
+			  :domain (ag:text server-domain-textbox)
+			  :resource (ag:text resource-textbox)
+			  :password (ag:text password-textbox)
+			  :mechanism :sasl-plain))
+    (setf wait-label (ag:label-new-string window "Bitte warten..."))
+    (ag:disable-widget login-button)))
+
+(defclass register-window (agar-window)
+  ((upper-hbox)
+   (failure-notice-label)
+   (host-jid-textbox)
+   (register-button)
+   (lower-hbox)
+   (wait-label)))
+
+(defmethod initialize-instance :after ((register-window register-window) &key)
+  "Initilializes the register window by creating the widgets etc."
+    (with-slots (window upper-hbox failure-notice-label
+			host-jid-textbox register-button lower-hbox
+			wait-label)
+	register-window
+      (setf window (ag:window-new :modal :noclose :nominimize))
+      (setf upper-hbox (ag:hbox-new window)
+	    failure-notice-label
+	    (ag:label-new-string (cffi:null-pointer)
+				 "Host akzeptierte die Registrierungsanfrage nicht! Wählen Sie einen anderen Host.")
+	    host-jid-textbox (ag:textbox-new window :label-text "Jabber-ID des Hostes: ")
+	    lower-hbox (ag:hbox-new window)
+	    wait-label (ag:label-new-string (null-pointer) "Warte auf Antwort des Hostes..."))
+      (setf register-button (ag:button-new lower-hbox nil "Registrierung anfragen"))
+      (ag:set-event register-button "button-pushed"
+		    (event-handler #'(lambda (event)
+				       (declare (ignore event))
+				       (send-registration-data (module register-window))))
+		    "")))
+
 (defun init-register-window (module)
   (setf (register-window module) (make-instance 'register-window :module module)))
+
+(defmethod query-registration ((register-window register-window))
+  (with-slots (upper-hbox lower-hbox failure-notice-label wait-label register-button)
+      register-window
+    (ensure-detached failure-notice-label upper-hbox)
+    (ensure-detached wait-label lower-hbox)
+    (ag:enable-widget register-button)))
+
+(defmethod send-registration-data ((module login-and-register))
+  "Sends the registration data back to kernel and gets the data from register-window"
+  (with-slots (window upper-hbox failure-notice-label host-jid-textbox wait-label lower-hbox register-button)
+      (register-window module)
+    (ensure-detached failure-notice-label upper-hbox)
+    (call-kernel-handler (ui module) 'registration-data
+			 (comm::make-xmpp-registration-data
+			  :host-jid (ag:text host-jid-textbox)))
+    (ensure-attached wait-label lower-hbox)
+    (ag:disable-widget register-button)
+    (ag:window-update window)))
+
+(defmethod registration-denied ((register-window register-window))
+  (with-slots (window upper-hbox failure-notice-label wait-label register-button)
+      register-window
+    (ag:enable-widget register-button)
+    (ag:attach-object upper-hbox failure-notice-label)
+    (ag:detach-object wait-label)
+    (ag:window-update window)))
 
 (defmethod initialize-instance :after ((module login-and-register)
 				       &key)
@@ -106,28 +175,14 @@
   (show (login-window module)))
 
 (defmethod query-registration ((module login-and-register))
-  "Shows the register-window"
+  "Hides the login-window and shows the register-window"
+  (hide (login-window module))
+  (query-registration (register-window module))
   (show (register-window module)))
 
-(defmethod send-login-data ((module login-and-register))
-  (declare (optimize debug))
-  (let ((widgets (widgets (login-window module))))
-    (call-kernel-handler (ui module) 'login-data
-			 (comm::make-xmpp-login-data
-			  :username (ag:text (cdr (assoc :username widgets)))
-			  :hostname (ag:text (cdr (assoc :hostname widgets)))
-			  :domain (ag:text (cdr (assoc :domain widgets)))
-			  :resource (ag:text (cdr (assoc :resource widgets)))
-			  :password (ag:text (cdr (assoc :password widgets)))
-			  :mechanism :sasl-plain))
-    (with-slots (widgets window) (login-window module)
-      (let* ((wait-label (ag:label-new-string window "Bitte warten...")))
-	(push (cons :wait-label wait-label) widgets)
-	(ag:disable-widget (cdr (assoc :login-btn widgets)))))))
+(defmethod registration-accepted ((module login-and-register))
+  "Hides the register-window"
+  (hide (register-window module)))
 
-(defmethod send-registration-data ((module login-and-register))
-  "Sends the registration data back to kernel and gets the data from register-window"
-  (let ((widgets (widgets (register-window module))))
-    (call-kernel-handler (ui module) 'registration-data
-			 (comm::make-xmpp-registration-data
-			  :host-jid (ag:text (cdr (assoc :host-jid widgets)))))))
+(defmethod registration-denied ((module login-and-register))
+  (registration-denied (register-window module)))
--- ui/opengl-ui/package.lisp
+++ ui/opengl-ui/package.lisp
@@ -1,4 +1,4 @@
 (defpackage skat-opengl-ui
   (:nicknames opengl-ui gl-ui)
-  (:shadowing-import-from cffi defcallback)
+  (:shadowing-import-from cffi defcallback null-pointer pointer-eq)
   (:use :cl :skat-ui :skat-utils))
