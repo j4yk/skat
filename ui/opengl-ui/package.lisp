3db2e8cc848587a242491b7e79594f3f496c8f4b
Jakob Reschke <jakres@gmail.com>
2010-03-20 00:52:37 +0100

    export gl-ui:opengl-ui

--- ui/opengl-ui/package.lisp
+++ ui/opengl-ui/package.lisp
@@ -2,4 +2,5 @@
   (:nicknames opengl-ui gl-ui)
   (:use :cl :skat-ui :skat-utils :cffi)
   (:shadow #:callback)
-  (:shadowing-import-from cffi defcallback null-pointer pointer-eq foreign-alloc foreign-free lisp-string-to-foreign))
+  (:shadowing-import-from cffi defcallback null-pointer pointer-eq foreign-alloc foreign-free lisp-string-to-foreign)
+  (:export #:opengl-ui))
