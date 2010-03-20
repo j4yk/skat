71a0f4421797a9f5fe8c44fd8e578609757b7647
Jakob Reschke <jakres@gmail.com>
2010-03-20 01:42:56 +0100

    added #'start-host and #'start-player convenience functions

--- start/start.lisp
+++ start/start.lisp
@@ -36,3 +36,9 @@
 			       (:host 'kern:host))
 	   ui-implementation comm-implementation
 	   kernel-initargs)))
+
+(defun start-host (login-data)
+  (start-skat :host 'ui:host-ui 'comm:xmpp-comm :login-data login-data))
+
+(defun start-gl-ui-player ()
+  (start-skat :player 'gl-ui:opengl-ui 'comm:xmpp-comm))
