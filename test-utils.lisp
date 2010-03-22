418fb951ecbf56a5edb8dafeb95fecdb39fba215
Jakob Reschke <jakres@gmail.com>
2010-03-22 19:57:30 +0100

    added test-host-login-data function

--- test-utils.lisp
+++ test-utils.lisp
@@ -4,3 +4,8 @@
   (make-xmpp-login-data :username name :password "skat"
 			:hostname "draugr.de" :domain ""
 			:resource resource :mechanism :sasl-plain))
+
+(defun test-host-login-data ()
+  (make-xmpp-login-data :username "skat" :password "skat"
+			:hostname "draugr.de" :domain ""
+			:resource "host" :mechanism :sasl-plain))
