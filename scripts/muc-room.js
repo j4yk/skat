70605ec5466c58833dd73763d9b2cf79d7b6d699
Jakob Reschke <jakres@gmail.com>
2010-10-23 20:18:05 +0200

    added Room.send_configuration

--- scripts/muc-room.js
+++ scripts/muc-room.js
@@ -1,19 +1,19 @@
 /* Skat.Comm.Room
-   implements generic MUC room actions
+implements generic MUC room actions
 
-   triggers the following events on itself:
-   - created               created the room
-   - joined                successfully joined the room
-   - left                  left the room
-   - nick-changed          nickname has changed
-   - error                 an error was send back
-   - error-while-joining   an error occurred when trying to join
-   - presence              some presence arrived
-   - user-joined           a user joined the room
-   - user-left             a user left the room
-   - awaits-configuration  room-configuration form received and prefilled
-   - room-configured       successfully configured the room
-   - error-while-configuring   an error was send back when the configuration form should be received
+triggers the following events on itself:
+- created               created the room
+- joined                successfully joined the room
+- left                  left the room
+- nick-changed          nickname has changed
+- error                 an error was send back
+- error-while-joining   an error occurred when trying to join
+- presence              some presence arrived
+- user-joined           a user joined the room
+- user-left             a user left the room
+- awaits-configuration  room-configuration form received and prefilled
+- room-configured       successfully configured the room
+- error-while-configuring   an error was send back when the configuration form should be received
 */
 Skat.Comm.Room = function () {
 	this.room_jid = null;
@@ -22,116 +22,135 @@ Skat.Comm.Room = function () {
 
 	this.joined = false;
 	this.roster = {};
+
+	this.config_form = null;
 };
 
-Skat.Comm.Room.prototype.join = function (my_jid, password, initial_presence) {
-	/// attempts to join a room by sending initial presence
-	/// sets room_jid, nick and my_jid
-	this.room_jid = Strophe.getBareJidFromJid(my_jid);
-	this.nick = Strophe.getResourceFromJid(my_jid);
-	this.my_jid = my_jid;
-	var room = this;
-	Skat.Comm.connection.addHandler(function (presence) {
-		return room.presence_handler(presence);
-	}, null, 'presence', null, null, this.room_jid, {matchBare: true});
-	var pres = null;
-	if (!initial_presence) {
-		pres = $pres({to: this.my_jid}).c("x", {xmlns: Strophe.NS.MUC});
-		if (password) {
-			pres.c("password").t(password); // append password element to x element
+Skat.Comm.Room.prototype = {
+	join: function (my_jid, password, initial_presence) {
+		/// attempts to join a room by sending initial presence
+		/// sets room_jid, nick and my_jid
+		this.room_jid = Strophe.getBareJidFromJid(my_jid);
+		this.nick = Strophe.getResourceFromJid(my_jid);
+		this.my_jid = my_jid;
+		var room = this;
+		Skat.Comm.connection.addHandler(function (presence) {
+			return room.presence_handler(presence);
+		}, null, 'presence', null, null, this.room_jid, {matchBare: true});
+		var pres = null;
+		if (!initial_presence) {
+			pres = $pres({to: this.my_jid}).c("x", {xmlns: Strophe.NS.MUC});
+			if (password) {
+				pres.c("password").t(password); // append password element to x element
+			}
 		}
-	}
-	Skat.Comm.connection.send(initial_presence || pres);
-};
+		Skat.Comm.connection.send(initial_presence || pres);
+	},
 
-Skat.Comm.Room.prototype.leave = function () {
-	// leave the room by sending unavailable presence
-	Skat.Comm.connection.send($pres({to: this.my_jid, type: 'unavailable'}));
-};
+	leave: function () {
+		// leave the room by sending unavailable presence
+		Skat.Comm.connection.send($pres({to: this.my_jid, type: 'unavailable'}));
+	},
 
-Skat.Comm.Room.prototype.configure = function (enforced_values, default_values, create_instant_room) {
-	// request the room configuration form
-	if (create_instant_room) {
-		// send an empty form
-		Skat.Comm.connection.sendIQ($iq({to: this.room_jid, type: 'set'}).c('query', {xmlns: Strophe.NS.MUC + "#owner"}).
+	configure: function (enforced_values, default_values, create_instant_room) {
+		// request the room configuration form
+		if (create_instant_room) {
+			// send an empty form
+			Skat.Comm.connection.sendIQ($iq({to: this.room_jid, type: 'set'}).c('query', {xmlns: Strophe.NS.MUC + "#owner"}).
 			c('x', {xmlns: 'jabber:x:data', type: 'submit'}), function (iq) {
-			// success
+				// success
+				$(this).trigger('room-configured');
+			}, function (iq) {
+				// error
+				$(this).trigger('error-while-configuring', { reason: iq === null ? 'timeout' : iq });
+			}, 10000);
+		} else {
+			// get the configuration form
+			Skat.Comm.connection.sendIQ($iq({to: this.room_jid, type: 'get'}).c('query', {xmlns: Strophe.NS.MUC + "#owner"}), function (iq) {
+				// form retrieved
+				if ($(iq).attr('type' === 'result') && $(iq).find('x[xmlns="jabber:x:data"][type="form"]').length > 0) {
+					// prefill form and display the rest to the user
+					var form = new Skat.Comm.XMPP.DataForm($(iq).find('x[xmlns="jabber:x:data"]'));
+					this.config_form = form;
+					form.set(enforced_values).set(default_values);
+					var subset = form.filter(function () {
+						return !enforced_values.hasOwnProperty($(this).attr('var'));
+					});
+					$(this).trigger('awaits-configuration', subset);
+				} else {
+					$(this).trigger('error-while-configuring', { reason: 'no configuration form' });
+				}
+			}, function (iq) {
+				// error
+				$(this).trigger('error-while-configuring', { reason: iq === null ? 'timeout' : iq });
+			}, 10000);
+		}
+	},
+
+	send_configuration: function (user_filled_form) {
+		// merge the user filled form into the received form
+		this.config_form.merge(user_filled_form, true);
+		// turn the form into a submit
+		Skat.Comm.connection.sendIQ($iq({type: 'set', to: this.room_jid}).c('query', {xmlns: Strophe.NS.MUC + '#owner'})
+		.cnode(this.config_form.submit()), function (iq) {
+			// successfully configured
 			$(this).trigger('room-configured');
 		}, function (iq) {
 			// error
 			$(this).trigger('error-while-configuring', { reason: iq === null ? 'timeout' : iq });
 		}, 10000);
-	} else {
-		// get the configuration form
-		Skat.Comm.connection.sendIQ($iq({to: this.room_jid, type: 'get'}).c('query', {xmlns: Strophe.NS.MUC + "#owner"}), function (iq) {
-			// form retrieved
-			if ($(iq).attr('type' === 'result') && $(iq).find('x[xmlns="jabber:x:data"][type="form"]').length > 0) {
-				// prefill form and display the rest to the user
-				var form = new Skat.Comm.XMPP.DataForm($(iq).find('x[xmlns="jabber:x:data"]'));
-				form.set(enforced_values).set(default_values);
-				var subset = form.filter(function () {
-					return !($(this).attr('var') in enforced_values);
-				});
-				$(this).trigger('awaits-configuration', subset);
-			} else {
-				$(this).trigger('error-while-configuring', { reason: 'no configuration form' });
-			}
-		}, function (iq) {
-			// error
-			$(this).trigger('error-while-configuring', { reason: iq === null ? 'timeout' : iq });
-		}, 10000);
-	}
-};
+	},
 
-Skat.Comm.Room.prototype.presence_handler = function (presence) {
-	/// this is the default presence handler for MUC rooms that are managed by Skat.Comm.Room
-	var from = $(presence).attr('from');
-	var type = $(presence).attr('type');
+	presence_handler: function (presence) {
+		/// this is the default presence handler for MUC rooms that are managed by Skat.Comm.Room
+		var from = $(presence).attr('from');
+		var type = $(presence).attr('type');
 
-	if ($(presence).find('status[code="210"]').length > 0) {
-		// server changed our nick
-		this.my_jid = from;
-		$(this).trigger('nick-changed', presence);
-	}
-
-	if (type === 'error') {
-		if (!this.joined) {
-			$(this).trigger('error-while-joining', presence);
-		} else {
-			$(this).trigger('error', presence);
+		if ($(presence).find('status[code="210"]').length > 0) {
+			// server changed our nick
+			this.my_jid = from;
+			$(this).trigger('nick-changed', presence);
 		}
-	} else if (from === this.my_jid || $(presence).find('status[code="110"]').length > 0) {
-		// our own presence
-		if (type === 'unavailable') {
-			// officially logged out
-			this.joined = false;
-			$(this).trigger('left', presence);
-			return false; // hence, this handler is obsolete
-		} else if ($(presence).find('status[code="110"], status[code="201"]').length > 0) {
+
+		if (type === 'error') {
 			if (!this.joined) {
-				this.joined = true;
-				if ($(presence).find('status[code="201"]').length > 0) {
-					$(this).trigger('created', presence);
+				$(this).trigger('error-while-joining', presence);
+			} else {
+				$(this).trigger('error', presence);
+			}
+		} else if (from === this.my_jid || $(presence).find('status[code="110"]').length > 0) {
+			// our own presence
+			if (type === 'unavailable') {
+				// officially logged out
+				this.joined = false;
+				$(this).trigger('left', presence);
+				return false; // hence, this handler is obsolete
+			} else if ($(presence).find('status[code="110"], status[code="201"]').length > 0) {
+				if (!this.joined) {
+					this.joined = true;
+					if ($(presence).find('status[code="201"]').length > 0) {
+						$(this).trigger('created', presence);
+					} else {
+						$(this).trigger('joined', presence);
+					}
+				}
+			}
+		} else {
+			// someone else's presence
+			var nick = Strophe.getResourceFromJid(from);
+			if (type === 'unavailable') {
+				delete this.roster[nick];
+				$(this).trigger('user-left', {nick: nick, jid: from, presence: presence});
+			} else {
+				if (!this.roster[nick]) {
+					this.roster[nick] = presence;
+					$(this).trigger('user-joined', presence);
 				} else {
-					$(this).trigger('joined', presence);
+					this.roster[nick] = presence;
+					$(this).trigger('user-presence', presence);
 				}
 			}
 		}
-	} else {
-		// someone else's presence
-		var nick = Strophe.getResourceFromJid(from);
-		if (type === 'unavailable') {
-			delete this.roster[nick];
-			$(this).trigger('user-left', {nick: nick, jid: from, presence: presence});
-		} else {
-			if (!this.roster[nick]) {
-				this.roster[nick] = presence;
-				$(this).trigger('user-joined', presence);
-			} else {
-				this.roster[nick] = presence;
-				$(this).trigger('user-presence', presence);
-			}
-		}
+		return true;
 	}
-	return true;
-};
+}
